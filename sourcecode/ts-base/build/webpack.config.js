f8bd461debf767ed078d718342d739b9b622791c
liangxiao <liangxiao@sogou-inc.com>
2019-07-17 15:26:26 +0800

    01-13

---
+++ sourcecode/ts-base/build/webpack.config.js
@@ -0,0 +1,8 @@
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.config')
+const devConfig = require('./webpack.dev.config')
+const proConfig = require('./webpack.pro.config')
+
+let config = process.NODE_ENV === 'development' ? devConfig : proConfig
+
+module.exports = merge(baseConfig, config)
