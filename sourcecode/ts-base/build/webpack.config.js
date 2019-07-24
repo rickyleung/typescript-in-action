dbe3cef693d72b3aeb5aef86c2071a58309bbf3a
liangxiao <liangxiao@sogou-inc.com>
2019-07-24 18:56:34 +0800

    11-17

--- sourcecode/ts-base/build/webpack.config.js
+++ sourcecode/ts-base/build/webpack.config.js
@@ -3,11 +3,6 @@ const baseConfig = require('./webpack.base.config')
 const devConfig = require('./webpack.dev.config')
 const proConfig = require('./webpack.pro.config')
 
-// Wrong!
-// let config = process.NODE_ENV === 'development' ? devConfig : proConfig
-// module.exports = merge(baseConfig, config)
+let config = process.NODE_ENV === 'development' ? devConfig : proConfig
 
-module.exports = (env, argv) => {
-    let config = argv.mode === 'development' ? devConfig : proConfig;
-    return merge(baseConfig, config);
-};
+module.exports = merge(baseConfig, config)
