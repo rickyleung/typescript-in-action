3719db50bdd8db4f07cbb430cbb41b4afb92bff4
rickyleung <43517912@qq.com>
2019-09-02 11:01:14 +0800

    Hooks version

--- sourcecode/ts-react-app/src/components/employee/index.tsx
+++ sourcecode/ts-react-app/src/components/employee/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Table } from 'antd';
 
 import './index.css';
@@ -7,6 +7,29 @@ import QueryForm from './QueryForm';
 
 import { employeeColumns } from './colums';
 import { EmployeeResponse } from '../../interface/employee';
+
+// Hooks version
+// const Employee = () => {
+//     const [employee, setEmployee] = useState<EmployeeResponse>(undefined);
+
+//     const getTotal = () => {
+//         let total: number;
+//         if (typeof employee !== 'undefined') {
+//             total = employee.length
+//         } else {
+//             total = 0
+//         }
+//         return <p>共 {total} 名员工</p>
+//     }
+
+//     return (
+//         <>
+//             <QueryForm onDataChange={setEmployee} />
+//             {/* {getTotal()} */}
+//             <Table columns={employeeColumns} dataSource={employee} className="table" />
+//         </>
+//     )
+// }
 
 interface State {
     employee: EmployeeResponse
